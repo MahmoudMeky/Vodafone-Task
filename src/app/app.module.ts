72339ea99e8d7994457d3d136f09413835b2865d
Mahmoud Meky <mahmoudmeky2017@gmail.com>
2024-01-12 00:17:16 +0200

    Simple changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { PostComponent } from './Components/post/post.component';
+import { MainLayoutComponent } from './Layouts/main-layout/main-layout.component';
+import { HomeComponent } from './Components/home/home.component';
+import { CommentComponent } from './Components/comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostComponent,
+    MainLayoutComponent,
+    HomeComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
